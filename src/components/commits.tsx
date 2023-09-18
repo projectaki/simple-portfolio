2c47c9bb243c419f37ee6b57d45052d6858c4bc0
Akos <akosegypro@gmail.com>
2023-09-18 21:31:59 +0200

    feat: add last commit

--- src/components/commits.tsx
+++ src/components/commits.tsx
@@ -1,37 +1,44 @@
 "use client";
 
 import { useCallback, useEffect, useState } from "react";
-import { getTop5RecentlyUsedRepos } from "../../lib/gh";
 
 export function Commits() {
-  // const [loading, setLoading] = useState(false);
-  // const [commit, setCommit] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [commit, setCommit] = useState(null);
 
-  // const runFunction = useCallback(async () => {
-  //   setLoading(true);
-  //   const commit = await getTop5RecentlyUsedRepos().catch((err) => {
-  //     setLoading(false);
-  //   });
+  const runFunction = useCallback(async () => {
+    setLoading(true);
+    try {
+      const commit = await fetch(
+        "https://gh_proxy.akosegypro.workers.dev"
+      ).then(async (res) => {
+        const data = await res.json();
+        return data;
+      });
+      setCommit(commit);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
 
-  //   setCommit(commit);
-
-  //   setLoading(false);
-  // }, []);
-
-  // useEffect(() => {
-  //   const interval = setInterval(runFunction, 60000);
-  //   runFunction();
-  //   return () => clearInterval(interval);
-  // }, [runFunction]);
+  useEffect(() => {
+    const interval = setInterval(runFunction, 60000);
+    runFunction();
+    return () => clearInterval(interval);
+  }, [runFunction]);
 
   return (
     <div className="mt-8">
-      {/* <h3 className="text-lg font-semibold mb-4 text-black">Last commit</h3>
+      <div className="flex items-center gap-x-2">
+        <h3 className="text-lg font-semibold text-black mb-0">Last commit</h3>
+        {loading ? <img src="/spinner.svg" alt="" /> : "just refreshed"}
+      </div>
+
       <ul className="bg-white   max-w-lg">
         <li className=" border-gray-300 py-2">
-          <span className="text-gray-600 text-sm">{commit}</span>
+          <span className="text-gray-600 text-sm">{commit?.commit}</span>
         </li>
-      </ul> */}
+      </ul>
     </div>
   );
 }
